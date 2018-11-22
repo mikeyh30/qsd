2282c2b7d6122ebaea9e1bc4d026a92289cb5b68
Gareth Jones <garethsion@googlemail.com>
2018-11-22 21:20:34 +0000

    Now with mocking modules

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,12 @@
 #
 import os
 import sys
+import mock
+ 
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
 #sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../../'))
 
